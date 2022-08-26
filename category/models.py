7579c19946ee2c3f2c395637efe4a90cbb85a57c
abdelrhmannabil12 <59992853+abdelrhmannabil12@users.noreply.github.com>
2022-08-26 16:56:52 +0200

    crated category model and registered it in admin

--- category/models.py
+++ category/models.py
@@ -1,3 +1,16 @@
+from tabnanny import verbose
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    category_name=models.CharField(max_length=100,unique=True)
+    slug=models.CharField(max_length=100,unique=True)
+    description=models.TextField(blank=True)
+    category_img=models.ImageField(upload_to='images/category',blank=True)
+
+    class Meta:
+        verbose_name='Categorie'
+
+
+    def __str__(self):
+        return self.category_name
