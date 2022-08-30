d301d9ae058a2f471d9068f5b1865c1d5d75271e
abdelrhmannabil12 <59992853+abdelrhmannabil12@users.noreply.github.com>
2022-08-30 15:01:19 +0200

    Created AccountAmin class to customiz admin panel and making the password read-only

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import *
 # Register your models here.
-admin.site.register(Account)
+class AcountAdmin(UserAdmin):
+    list_display=('email','first_name','last_name','username','date_joined','is_active')
+    list_display_links=('email','first_name','last_name','username','date_joined','is_active')
+    readonly_fields=('date_joined',)
+    ordering=('date_joined',)
+    fieldsets=()
+    list_filter=()
+    filter_horizontal=()
+admin.site.register(Account,AcountAdmin)
