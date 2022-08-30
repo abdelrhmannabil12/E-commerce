5a462eb5f8d959029d6e4db82cbdaaed3a93878e
abdelrhmannabil12 <59992853+abdelrhmannabil12@users.noreply.github.com>
2022-08-30 15:48:49 +0200

    created the product model in store app

--- store/models.py
+++ store/models.py
@@ -1,3 +1,23 @@
+from email.mime import image
+from itertools import product
+from traceback import print_exc
 from django.db import models
 
+from category.models import Category
+
 # Create your models here.
+class Product(models.Model):
+    product_name=models.CharField(max_length=100,unique=True)
+    slug=models.SlugField(max_length=100,unique=True)
+    description=models.TextField(max_length=500,blank=True)
+    price=models.IntegerField()
+    images=models.ImageField(upload_to='images/products')
+    stock=models.IntegerField()
+    is_available=models.BooleanField(default=True)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    created_date=models.DateTimeField(auto_now_add=True)
+    modified_date=models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.product_name
