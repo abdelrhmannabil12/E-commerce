e71b7ac5fb1fc634ad1e700bc1efd3377e6f2dda
abdelrhmannabil12 <59992853+abdelrhmannabil12@users.noreply.github.com>
2022-08-30 17:13:56 +0200

    added the home template to store app

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('category.urls'))
+    path('',include('store.urls')),
+
 ]
 urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
