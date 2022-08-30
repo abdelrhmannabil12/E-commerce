e84c6e6f471d7d2be002adb4f85ba06d71edd885
abdelrhmannabil12 <59992853+abdelrhmannabil12@users.noreply.github.com>
2022-08-30 18:27:34 +0200

    adding the slug to url to display products by category

--- store/urls.py
+++ store/urls.py
@@ -3,6 +3,6 @@ from . import views
 urlpatterns = [
 
 path('',views.home,name="home"),
-path('store/',views.store,name="store")
-
+path('store/',views.store,name="store"),
+path('store/<slug:category_slug>',views.store,name='products_by_category')
 ]
--- store/views.py
+++ store/views.py
@@ -1,6 +1,10 @@
 from itertools import product
-from django.shortcuts import render
+from tracemalloc import get_object_traceback
+from django.shortcuts import render,get_object_or_404
+
+from category.models import Category
 from .models import Product
+from category.models import *
 # Create your views here.
 
 def home(request):
@@ -8,8 +12,14 @@ def home(request):
 
     return render(request,'home.html',{'products':products})
 
-def store(request):
-    products=Product.objects.all().filter(is_available=True)
-    product_count=products.count()
+def store(request,category_slug=None):
+
+    if category_slug!=None:
+        categories=get_object_or_404(Category,slug=category_slug)
+        products=Product.objects.filter(category=categories,is_available=True)
+        product_count=products.count()
+    else:     
+        products=Product.objects.all().filter(is_available=True)
+        product_count=products.count()
     return render(request,"store.html",{'products':products,
                                         'count':product_count})
