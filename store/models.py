5d0402ce7d2a5046cde13e2898254ec1bb3891f5
abdelrhmannabil12 <59992853+abdelrhmannabil12@users.noreply.github.com>
2022-09-04 08:46:18 +0200

    getting the url with category slug and product slug

--- store/models.py
+++ store/models.py
@@ -1,10 +1,11 @@
+from ast import arg
 from email.mime import image
 from itertools import product
 from traceback import print_exc
 from django.db import models
 
 from category.models import Category
-
+from django.urls import reverse
 # Create your models here.
 class Product(models.Model):
     product_name=models.CharField(max_length=100,unique=True)
@@ -18,6 +19,8 @@ class Product(models.Model):
     created_date=models.DateTimeField(auto_now_add=True)
     modified_date=models.DateTimeField(auto_now=True)
 
-
+    @property
+    def get_url(self):
+        return reverse("product_detail",args=[self.category.slug,self.slug])
     def __str__(self):
         return self.product_name
