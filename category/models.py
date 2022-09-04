00b0910e63b6ddc84a3562a74d8dd1e7af17bc8c
abdelrhmannabil12 <59992853+abdelrhmannabil12@users.noreply.github.com>
2022-09-04 06:42:13 +0200

    adding the link that will display the categories by their slugs

--- category/models.py
+++ category/models.py
@@ -1,3 +1,4 @@
+from django.urls import reverse
 from tabnanny import verbose
 from django.db import models
 
@@ -10,7 +11,9 @@ class Category(models.Model):
 
     class Meta:
         verbose_name='Categorie'
-
+    @property
+    def get_url(self):
+        return reverse("products_by_category",args=[self.slug])
 
     def __str__(self):
         return self.category_name
