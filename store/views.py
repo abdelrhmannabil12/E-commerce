ee82a64c603b084c71d2fc023501b4551faf75ff
abdelrhmannabil12 <59992853+abdelrhmannabil12@users.noreply.github.com>
2022-09-04 08:23:05 +0200

    creating a url and view for single product

--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
 from itertools import product
 from tracemalloc import get_object_traceback
+from unicodedata import category
 from django.shortcuts import render,get_object_or_404
 
 from category.models import Category
@@ -23,3 +24,11 @@ def store(request,category_slug=None):
         product_count=products.count()
     return render(request,"store.html",{'products':products,
                                         'count':product_count})
+
+
+def product_detail(request,category_slug,product_slug):
+    try:
+        single_product=Product.objects.get(category__slug=category_slug,slug=product_slug)
+    except Exception as e:
+        raise e
+    return render(request,'product_detail.html',{"single_product":single_product})
