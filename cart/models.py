c70458c2a145233c1813c0815ecbaa5912459db6
abdelrhmannabil12 <59992853+abdelrhmannabil12@users.noreply.github.com>
2022-09-04 10:09:20 +0200

    created the cart and cart item models

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,23 @@
+from itertools import product
+from statistics import mode
 from django.db import models
-
+from store.models import *
 # Create your models here.
+class Cart(models.Model):
+    cart_id=models.CharField(max_length=200)
+    date_added=models.DateField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.cart_id
+
+
+class CartItem(models.Model):
+    cart=models.ForeignKey(Cart,on_delete=models.CASCADE)
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity=models.IntegerField()
+    is_active=models.BooleanField(default=True)
+
+
+    def __str__(self):
+        return self.product
