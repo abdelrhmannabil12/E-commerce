999279b42a6bbfc7e4c9978bebe46a96c61cd596
abdelrhmannabil12 <59992853+abdelrhmannabil12@users.noreply.github.com>
2022-09-05 15:17:59 +0200

    getting the cartitems of the customer cart

--- cart/views.py
+++ cart/views.py
@@ -33,5 +33,16 @@ def add_cart(request,product_id):
         cart_item.save()
     return redirect('cart')
 
-def cart(request):
-   return render(request,"cart.html")
+def cart(request,total=0,quantity=0,cart_item=None):
+    try:
+        cart=Cart.objects.get(cart_id=_cart_id(request))
+        cart_items=CartItem.objects.filter(cart=cart,is_active=True)
+        for cart_item in cart_items:
+            total+=(cart_item.product.price * cart_item.quantity)
+            quantity+=cart_item.quantity
+    except Cart.DoesNotExist:
+        pass
+   
+    return render(request,"cart.html",{'total':total,
+                                        'quantity':quantity,
+                                        'cart_item':cart_items    })
