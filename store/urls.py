85d122187654d6d88a61e6153caee8f6a34d196f
abdelrhmannabil12 <59992853+abdelrhmannabil12@users.noreply.github.com>
2022-11-29 17:15:38 +0200

    added search functionality

--- store/urls.py
+++ store/urls.py
@@ -6,5 +6,5 @@ path('',views.home,name="home"),
 path('store/',views.store,name="store"),
 path('store/<slug:category_slug>',views.store,name='products_by_category'),
 path('store/<slug:category_slug>/<slug:product_slug>/',views.product_detail,name='product_detail'),
-
+path('search/',views.search,name='search')
 ]
--- store/views.py
+++ store/views.py
@@ -2,13 +2,14 @@ from itertools import product
 from tracemalloc import get_object_traceback
 from unicodedata import category
 from django.shortcuts import render,get_object_or_404
-
+from django.http import HttpResponse
 from category.models import Category
 from .models import Product
 from category.models import *
 from cart.models import *
 from cart.views import _cart_id
 from django.core.paginator import *
+from django.db.models import Q
 # Create your views here.
 
 def home(request):
@@ -39,3 +40,12 @@ def product_detail(request,category_slug,product_slug):
     except Exception as e:
         raise e
     return render(request,'product_detail.html',{"single_product":single_product,"in_cart":in_cart})
+
+
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword=request.GET['keyword']
+        if keyword:
+            products=Product.objects.order_by("created_date").filter(Q(description__icontains=keyword) |  Q(product_name__icontains=keyword))
+    return render(request,'store.html',{'products':products})
