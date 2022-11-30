326bbf05087be214ee64b605004e68cf00838d20
abdelrhmannabil12 <59992853+abdelrhmannabil12@users.noreply.github.com>
2022-11-30 18:01:18 +0200

    login functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render,redirect
-from django.contrib import messages
+from django.contrib import messages,auth
 from .forms import RegisterationForm
 from .models import *
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def register(request):
     if request.method == 'POST':
@@ -20,6 +21,22 @@ def register(request):
         form=RegisterationForm()
     return render(request,'register.html',{"form":form})
 def login(request):
+    if request.method == 'POST':
+        email=request.POST['email']
+        password=request.POST['password']
+
+        user= auth.authenticate(email=email,password=password)
+        if user is not None:
+            auth.login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request,"wrong password or username , please try again")
+            return redirect('login')
     return render(request,'login.html')
+
+
+@login_required(login_url = 'login')
 def logout(request):
-    return 
+    auth.logout(request)
+    messages.success(request,"You are Logged out")
+    return redirect('login')
