a1c0a14458159b1576ccb6a708802edeb6a57b8c
abdelrhmannabil12 <59992853+abdelrhmannabil12@users.noreply.github.com>
2022-12-03 19:23:14 +0200

    verification massage after registeration

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render,redirect
 from django.contrib import messages,auth
 from .forms import RegisterationForm
 from .models import *
 from django.contrib.auth.decorators import login_required
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode,urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
 # Create your views here.
 def register(request):
     if request.method == 'POST':
@@ -15,11 +22,27 @@ def register(request):
             username=email.split('@')[0]
             user=Account.objects.create_user(first_name=first_name,last_name=last_name,email=email,username=username,password=password)
             user.save()
+
+
+            current_site=get_current_site(request)
+            mail_subject="Please Active Your account"
+            message=render_to_string('verification_email.html',{
+                'user':user,
+                'domain':current_site,
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+                'token':default_token_generator.make_token(user),
+            })
+            to_email=email 
+            send_email=EmailMessage(mail_subject,message,to=[to_email])
+            send_email.send()
             messages.success(request,'Registeration Successful')
             return redirect('register')
     else:
         form=RegisterationForm()
     return render(request,'register.html',{"form":form})
+
+
+
 def login(request):
     if request.method == 'POST':
         email=request.POST['email']
@@ -40,3 +63,19 @@ def logout(request):
     auth.logout(request)
     messages.success(request,"You are Logged out")
     return redirect('login')
+
+def activate(request,uidb64,token):
+    try:
+        uid=urlsafe_base64_decode(uidb64).decode()
+        user=Account._default_manager.get(pk=uid)
+    except(TypeError,ValueError,OverflowError,Account.DoesNotExist):
+        user=None
+
+    if user is not None and default_token_generator.check_token(user,token):
+        user.is_active=True
+        user.save()
+        messages.success(request,"Congratulations! your account is activated")
+        return redirect('login')
+    else:
+        messages.error(request,"Invalid Activation Link")
+        return redirect('register')
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -144,3 +144,10 @@ MESSAGE_TAGS = {
         messages.WARNING: 'alert-warning',
         messages.ERROR: 'alert-danger',
  }
+
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'abdelrhmannabilworking@gmail.com'
+EMAIL_HOST_PASSWORD = 'xsupamirexvualsx'
+EMAIL_USE_TLS = True
