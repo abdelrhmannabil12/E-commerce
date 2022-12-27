abe35ccfb80b7fa862376e2bda0ad3f181218a86
abdelrhmannabil12 <59992853+abdelrhmannabil12@users.noreply.github.com>
2022-12-27 21:06:26 +0200

    adding inline for product in order product table

--- orders/admin.py
+++ orders/admin.py
@@ -2,11 +2,17 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
+class OrderProductInline(admin.TabularInline):
+    model = OrderProduct
+    extra=0
+    readonly_fields=('payment','user','product','quantity','product_price','order',)
+
 class OrderAdmin(admin.ModelAdmin):
     list_display =['order_number','full_name','phone','email','city','order_total','is_ordered']
     list_filter=['status','is_ordered']
     search_fields=['order_number','first_name','last_name','phone','email']
     list_per_page=20
+    inlines=[OrderProductInline]
 admin.site.register(Payment)
 admin.site.register(Order,OrderAdmin)
 admin.site.register(OrderProduct)
