19f1d3ecb8b1157d411906658b1172b782127b6d
abdelrhmannabil12 <59992853+abdelrhmannabil12@users.noreply.github.com>
2022-12-31 12:35:38 +0200

    created UserProfile Model to get more info

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,8 @@
+from asyncio import format_helpers
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import *
+from django.utils.html import format_html
 # Register your models here.
 class AcountAdmin(UserAdmin):
     list_display=('email','first_name','last_name','username','date_joined','is_active')
@@ -10,4 +12,12 @@ class AcountAdmin(UserAdmin):
     fieldsets=()
     list_filter=()
     filter_horizontal=()
+class UserProfileAdmin(admin.ModelAdmin):
+    def thumbnail(self,object):
+        return format_html('<img src="{}" width="30" style="border-radius:50%;" >'.format(object.profile_picture.url))
+
+    thumbnail.short_description="Profile Picture"
+    list_display=('thumbnail','user','city','state','country')
+
 admin.site.register(Account,AcountAdmin)
+admin.site.register(UserProfile,UserProfileAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -66,3 +66,22 @@ class Account(AbstractBaseUser):
  
     def has_module_perms(self,add_label):
         return True
+
+class UserProfile(models.Model):
+    user=models.OneToOneField(Account,on_delete=models.CASCADE)
+    address_line_1=models.CharField(blank=True,max_length=100)
+    address_line_2=models.CharField(blank=True,max_length=100)
+    city=models.CharField(blank=True,max_length=100)
+    state=models.CharField(blank=True,max_length=100)
+    country=models.CharField(blank=True,max_length=100)
+    profile_picture=models.ImageField(blank=True,upload_to='userprofile')
+
+
+    def __str__(self) :
+        return self.user.first_name
+    
+
+    def full_addrerss(self):
+        return f'{self.address_line_1} {self.address_line_2}'
+    
+    
