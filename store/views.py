b0f68e220a3685e91aec056afc83bed5d9d92d72
abdelrhmannabil12 <59992853+abdelrhmannabil12@users.noreply.github.com>
2023-01-02 08:02:59 +0200

    showing the reviews in home page

--- store/views.py
+++ store/views.py
@@ -12,9 +12,12 @@ from django.contrib import messages
 from orders.models import *
 
 def home(request):
-    products=Product.objects.all().filter(is_available=True)
-
-    return render(request,'home.html',{'products':products}) 
+    products=Product.objects.all().filter(is_available=True).order_by('created_date')
+    for product in products:
+        reviews = ReviewRating.objects.filter(product_id=product.id, status=True)
+    context={'products':products,
+            'reviews':reviews}
+    return render(request,'home.html',context) 
 def store(request, category_slug=None):
     categories = None
     products = None
