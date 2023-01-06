ae94b2c96474f2e0dd50574b58632288aa52c7c3
abdelrhmannabil12 <59992853+abdelrhmannabil12@users.noreply.github.com>
2023-01-06 20:24:03 +0200

    railway deployment

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default=True,cast=bool)
 
-ALLOWED_HOSTS = ['ecommerceappenv.eba-5dxmssbg.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -82,25 +82,13 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 AUTH_USER_MODEL='accounts.Account'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-if 'RDS_DB_NAME' in os.environ:
-    DATABASES={
-        'default':{
-            'ENGINE':'django.db.backends.postgresql',
-            'NAME':os.environ['RDS_DB_NAME'],
-            'USER':os.environ['RDS_USERNAME'],
-            'PASSWORD':os.environ['RDS_PASSWORD'],
-            'HOST':os.environ['RDS_HOSTNAME'],
-            'PORT':os.environ['RDS_PORT'],
-        }
-    }
 
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
 
 
 # Password validation
@@ -140,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -18,7 +18,7 @@ from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('notadmin/', admin.site.urls),
     path('',include('store.urls')),
     path('cart/',include('cart.urls')),
     path('accounts/',include('accounts.urls')),
