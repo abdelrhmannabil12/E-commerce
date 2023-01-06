ed0356579058510e0bb9f27f95c2f8f54c46cd9c
abdelrhmannabil12 <59992853+abdelrhmannabil12@users.noreply.github.com>
2023-01-06 18:15:14 +0200

    fix an reviews issue

--- store/views.py
+++ store/views.py
@@ -13,6 +13,7 @@ from orders.models import *
 
 def home(request):
     products=Product.objects.all().filter(is_available=True).order_by('created_date')
+    reviews=None
     for product in products:
         reviews = ReviewRating.objects.filter(product_id=product.id, status=True)
     context={'products':products,
